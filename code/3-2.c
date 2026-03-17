40ab1c495fcbaff38695d4df102c7b79383a08cf
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-03-17 13:43:39 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main()
+int main() 
 {
+    int a = -1;
+    unsigned int b = 1;
+
+    if (a < b) {
+        printf("a < b\n");
+    }
+    else {
+        printf("a >= b ???\n");
+    }
 
     return 0;
-}
 
+    // a > b 의 결과가 나온다. int와 unsigned int가 가팅 연산되면 int가 unsigned int로 형변환이 일어나기 때문이다. 
+}
