e8ba1d2ec29186c549542869071f2c90369af3a9
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-03-17 15:31:16 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,32 @@
 
 int main()
 {
+    int a;
+    int b;
+    char cal;
+
+    scanf("%d %d %c", &a, &b, &cal);
+
+    if (cal == '+') {
+        printf("%d\n", a + b);
+    }
+    else if (cal == '-') {
+        printf("%d\n", a - b);
+    }
+    else if (cal == '*') {
+        printf("%d\n", a * b);
+    }
+    else if (cal == '/') {
+        if (b == 0) {
+            printf("0으로 나누기 불가");
+        }
+        else {
+            printf("%d\n", a / b);
+        }
+    }
+    else {
+        printf("잘못된 기호");
+    }
 
     return 0;
 }
-
