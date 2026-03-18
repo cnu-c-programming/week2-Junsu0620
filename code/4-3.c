7bf6e00b97061daf9722e04fa05c77f3041ce149
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-03-18 23:14:57 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 int main()
-{
+{   
+    int a;
 
+    scanf("%d", &a);
+
+    if (a <= 1) {
+        printf("false\n");
+        return 0;
+    }
+
+    for (int b = 2; b * b <= a; b++) {
+        if (a % b == 0) {
+            printf("false\n");
+            return 0;
+        }
+    }
+    
+    printf("true\n");
     return 0;
 }
-
