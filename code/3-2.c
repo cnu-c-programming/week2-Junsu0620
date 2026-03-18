b8f5b01bb402f72534bc542cadd110eb5018f0e5
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-03-18 22:38:55 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -9,10 +9,9 @@ int main()
         printf("a < b\n");
     }
     else {
-        printf("a > b\n");
+        printf("a >= b ???\n");
     }
 
     return 0;
 
-    // a > b 의 결과가 나온다. int와 unsigned int가 가팅 연산되면 int가 unsigned int로 형변환이 일어나기 때문이다. 
 }
